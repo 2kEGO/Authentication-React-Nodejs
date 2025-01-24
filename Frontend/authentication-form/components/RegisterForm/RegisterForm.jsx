e601c7b9937992f1fff22f4ec2efc0de1c88d70c
Long Nguyen <barnonguyen@gmail.com>
2025-01-24 18:21:09 -0500

    update

--- Frontend/authentication-form/components/RegisterForm/RegisterForm.jsx
+++ Frontend/authentication-form/components/RegisterForm/RegisterForm.jsx
@@ -1,13 +1,28 @@
+import './RegisterForm.css'
 import React, { useEffect, useState } from 'react';
 import {RegisterUser} from '../../services/authServices.js';
+
+import img from '../../public/10468707.jpg'
 
 
 
 export default function RegisterForm () {
 
+    const USER_REGEX = /^[A-z][A-z0-9-_]{4,24}$/;
+    const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,24}$/;
+
     const [user,setUser] = useState('')
+    const [validUser, setValidUser] = useState(false)
+    const [userFocus, setUserFocus] = useState(false);
+
     const [pwd, setPwd] = useState('')
+    const [validPwd, setValidPwd] = useState(false)
+    const [pwdFocus, setPwdFocus] = useState(false);
+
     const [confirmPwd, setConfirmPwd] = useState('')
+    const [validConfirmPwd, setValidConfirmPwd] = useState(false)
+    const [confirmPwdFocus, setConfirmPwdFocus] = useState(false);
+
     const [accountCreated, setAccountCreated] = useState(false)
 
 
@@ -36,45 +51,84 @@ export default function RegisterForm () {
         }
     },[accountCreated]);
 
+    useEffect(() => {
+        setValidUser(USER_REGEX.test(user))
+    },[user]);
+
+    useEffect(() => {
+        setValidPwd(PWD_REGEX.test(pwd))
+        setValidConfirmPwd(pwd === confirmPwd);
+    },[pwd,confirmPwd]);
+
     return (
         <>
-            <div className="register-form-container">1
-                <form onSubmit={handleSubmit} >
+            <div className="register-form-container">
+                <div className="register-form-wrapper">
+                    <form onSubmit={handleSubmit} >
 
-                    <label htmlFor="user">UserName</label>
-                    <input 
-                    
-                        type="text"
-                        id="user"
-                        required={true}
-                        value={user}
-                        onChange={(e) => setUser(e.target.value)}
+                        <div className='input-box'>
+                            <label htmlFor="user">UserName</label>
+                            <input 
+                            
+                                type="text"
+                                id="user"
+                                required={true}
+                                value={user}
+                                onChange={(e) => setUser(e.target.value)}
+                                onFocus={()=> {setUserFocus(true)}}
+                                onBlur={()=> {setUserFocus(false)}}
+                            />
+                            <p id='uid' className={!validUser && userFocus? 'show' : 'hidden'}>
+                                4 to 24 characters <br />
+                                Must begin with a letter <br />
+                                Special characters are not  allowed
+                            </p>
 
-                    />
-                    
-                    <label htmlFor="pwd">Password</label>
-                    <input 
-                        type="text"
-                        id="pwd"
-                        required={true}
-                        value={pwd}
-                        onChange={(e) => setPwd(e.target.value)}
+                        </div>
+
+                        <div className='input-box'>
+                            <label htmlFor="pwd">Password</label>
+                            <input 
+                                type="text"
+                                id="pwd"
+                                required={true}
+                                value={pwd}
+                                onChange={(e) => setPwd(e.target.value)}
+                                onFocus={() => {setPwdFocus(true)}}
+                                onBlur={() => {setPwdFocus(false)}}
+                            />
+                            <p id='pwdid' className={!validPwd && pwdFocus? 'show' : 'hidden'}>
+                                8 to 24 characters.<br />
+                                Must include uppercase and lowercase letters, a number and a special character.<br />
+                            </p>
+                            
+                        </div>
                         
-                    />
-                    
-                    <label htmlFor="confirmPwd">Confirm Password</label>
-                    <input                    
-                        type="text"
-                        id="confirmPwd"
-                        required={true}
-                        value={confirmPwd}
-                        onChange={(e) => setConfirmPwd(e.target.value)}
-                        
-                    />
+                        <div className='input-box'>
+                            <label htmlFor="confirmPwd">Confirm Password</label>
+                            <input                    
+                                type="text"
+                                id="confirmPwd"
+                                required={true}
+                                value={confirmPwd}
+                                onChange={(e) => setConfirmPwd(e.target.value)}
+                                onFocus={()=> setConfirmPwdFocus(true)}
+                                onBlur={()=> setConfirmPwdFocus(false)}
+                            />
 
-                    <button type="submit">Submit</button>
+                            <p id='pwdConfirmId' className={!validConfirmPwd && confirmPwdFocus? 'show': 'hidden'}>
+                                Password does not match
+                            </p>
 
-                </form>
+                        </div>
+                        <button type="submit" disabled={!validUser && !validPwd && !validConfirmPwd? true: false}>Submit</button>
+
+                    </form>
+
+                    <div id='img-container'>
+                        <img src={img} alt="" id='img'/>
+                    </div>
+                </div>
             </div>
         </>
     )
